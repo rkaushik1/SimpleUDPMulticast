7e9e37a7499cee9583b13200fc133bccfd1919a5
Kaushik Rao <rkaushik@umich.edu>
2019-05-18 13:38:03 -0500

    Templates in one file

--- SimpleQueue.h
+++ SimpleQueue.h
@@ -11,10 +11,23 @@ private:
 	std::mutex queue_mutex;
 
 public:
-	void push(T const & data);
-	bool empty();
-	T front();
-	void pop();
-};
+	void push(T const & data){
+		std::lock_guard<std::mutex> lock(this->queue_mutex);
+		this->sub_queue.push(data);
+	}
 
-#include "SimpleQueue.cpp"
+	bool empty(){
+		std::lock_guard<std::mutex> lock(this->queue_mutex);
+		return this->sub_queue.empty();
+	}
+
+	T front(){
+		std::lock_guard<std::mutex> lock(this->queue_mutex);
+		return this->sub_queue.front();
+	}
+
+	void pop(){
+		std::lock_guard<std::mutex> lock(this->queue_mutex);
+		this->sub_queue.pop();
+	}
+};
